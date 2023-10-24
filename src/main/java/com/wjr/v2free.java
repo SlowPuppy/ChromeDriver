53eff8f015cf89515dd9737648a53116453a6be9
SlowPuppy <877390550@qq.com>
2023-10-25 07:57:43 +0800

    feat:修改配置2

--- src/main/java/com/wjr/v2free.java
+++ src/main/java/com/wjr/v2free.java
@@ -14,14 +14,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class v2free {
     public static void main(String[] args) throws Exception {
         // EdgeDriver driver = new EdgeDriver();
-        WebDriver driver=new ChromeDriver();
         ChromeOptions chrome_options=new ChromeOptions();
-
+        
         chrome_options.addArguments("--no-sandbox") ;//解决DevToolsActivePort文件不存在的报错
         chrome_options.addArguments("window-size=1920x1080"); // 指定浏览器分辨率
         chrome_options.addArguments("--disable-gpu") ;// 谷歌文档提到需要加上这个属性来规避bug
         chrome_options.addArguments("--headless") ;// 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-
+        
+        WebDriver driver=new ChromeDriver();
         String userName = args[0];
         String passWord = args[1];
         System.out.println("userName=" + userName + "passWord="+passWord);
